6c91814999ba8053e6e842accaea180500c60537
3k-dome <doenerfresser3k@gmail.com>
2022-08-14 22:24:14 +0200

    added small util print

--- util/__init__.py
+++ util/__init__.py
@@ -1,2 +1,10 @@
+from flask import Flask
 from .database import load_words, setup_difficulties
 from .settings import load_settings_file
+
+
+def print_map(app: Flask) -> None:
+    print("Routes:".ljust(59, "-"))
+    for rule in app.url_map.iter_rules():
+        print(f"{rule}".ljust(25), "\t", rule.methods)
+    print("-" * 59, end="\n\n")
