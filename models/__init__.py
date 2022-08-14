e6f939b47032e6f40a157e8b67d3429171377ee7
3k-dome <doenerfresser3k@gmail.com>
2022-08-14 12:01:22 +0200

    small doc changes

--- models/__init__.py
+++ models/__init__.py
@@ -11,6 +11,7 @@ from .score_model import Score
 from .user_model import User
 from .word_model import Word
 
-# this makes 'from models import *' import all models
+# this makes our models available as top level imports and as '*'
+# e.g. 'from models import * | *class*' instead of 'from models.*module* import *class*'
 
 __all__ = ["Difficulty", "Score", "User", "Word"]
